d0100a1b1728b4a1efa18a806ed317bf22b92a16
Harshang Virani <harshang.008@gmail.com>
2023-11-02 16:28:32 +0530

    Changes done.

--- wings-dialogs/build.gradle.kts
+++ wings-dialogs/build.gradle.kts
@@ -35,18 +35,13 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.3"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
+        kotlinCompilerExtensionVersion = "1.5.30"
     }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
